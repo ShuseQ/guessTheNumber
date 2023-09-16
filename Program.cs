5fe53ddbf51f410c406e5b670cc7e1b00ccf4f37
Santa Claus <artm.legay.06@inbox.ru>
2023-09-16 18:33:50 +0300

    sds

--- Program.cs
+++ Program.cs
@@ -10,17 +10,35 @@ namespace Lesson
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите возраст: ");
-            int age = 22;
-            
-            if (age >= 18)
+            int number;
+            int lower, higher;
+            int tries = 5;
+            int userInput;
+            Random rand = new Random();
+
+            number = rand.Next(0, 101);
+            lower = rand.Next(number - 10, number);
+            higher = rand.Next(number + 1, number + 10);
+
+            Console.WriteLine($"Мы загадали число от 0 до 100, оно больше чем {lower} и меньше чем {higher}.");
+            Console.WriteLine($"Что это за число? У вас {tries} попыток.");
+
+            while (tries-- > 0)
             {
-                Console.WriteLine("Вы можете пройти!");
+                Console.Write("Ваш ответ: ");
+                userInput = Convert.ToInt32(Console.ReadLine());
+
+                if (userInput == number)
+                {
+                    Console.WriteLine("Правильный ответ, ответ: " + number + ".");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Вы не угадали. Попробуйте снова!");
+                }
             }
-            else
-            {
-                Console.WriteLine("Вы слишком юны, приходите через " + (18 - age) + " лет!");
-            }
+            Console.WriteLine("Вы проиграли. Выбранное число: " + number);
         }
     }
 }
